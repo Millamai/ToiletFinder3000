fcf21fca395f1f6ba2216bb3299a0f720a76e4e9
Camilla Mai Ryskjær <CARY@zealand.dk>
2026-04-13 10:39:12 +0200

    ToiletService has been added

---
+++ ToiletFinder3000/ToiletService.cs
@@ -0,0 +1,24 @@
+﻿using ToiletFinder3000.Model;
+using ToiletFinder3000.Repository;
+
+namespace ToiletFinder3000
+{
+    public class ToiletService
+    {
+
+
+        private readonly IToiletRepository _toiletRepository;
+
+        public ToiletService(IToiletRepository repo)
+        {
+            _toiletRepository = repo;
+        }
+
+
+
+        public List<Toilet> GetAll()
+        {
+            return _toiletRepository.GetAll();
+        }
+    }
+}
